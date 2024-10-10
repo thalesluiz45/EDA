0af89495cb99eea2a067381821821668215fabaa
thalesluiz45 <thalesluiz45@hotmail.com>
2024-10-09 23:10:32 -0300

    Ordenar Lista

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(1);
+        numeros.add(5);
+        numeros.add(4);
+
+        System.out.println(numeros);
+
+        for (int i = 0; i < numeros.size() - 1; i++) {
+            for (int j = 0; j < numeros.size() - 1 - i; j++) {
+                if (numeros.get(j) > numeros.get(j + 1)) {
+
+                    int temp = numeros.get(j);
+                    numeros.set(j, numeros.get(j + 1));
+                    numeros.set(j + 1, temp);
+                }
+            }
+        }
+
+        System.out.println(numeros);
+    }
+}
